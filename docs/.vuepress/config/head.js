465f4669965d77e2fcf7aaeb45845d6bc606784c
everywhere.z <weenyc31@163.com>
2022-06-07 23:08:06 +0800

    submit

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -12,7 +12,7 @@ module.exports = [
     // https://github.com/bytegravity/whitelist-wwads
     function ABDetected() {
       var adBlockDetected_div = document.createElement("div");
-      adBlockDetected_div.style.cssText = "position: sticky; top: 0; left: 0; width: 100%; background: #E01E5A; color: #fff; z-index: 9999999999; font-size: 14px; text-align: center; line-height: 1.5; font-weight: bold; padding-top: 6px; padding-bottom: 6px;";
+      adBlockDetected_div.style.cssText = "position: absolute; top: 0; left: 0; width: 100%; background: #E01E5A; color: #fff; z-index: 9999999999; font-size: 14px; text-align: center; line-height: 1.5; font-weight: bold; padding-top: 6px; padding-bottom: 6px;";
       adBlockDetected_div.innerHTML = "我们的广告服务商 <a style='color:#fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/end-user-privacy'>并不跟踪您的隐私</a>，为了支持本站的长期运营，请将我们的网站 <a style='color: #fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/whitelist-wwads'>加入广告拦截器的白名单</a>。";
       document.getElementsByTagName("body")[0].appendChild(adBlockDetected_div);
       // add a close button to the right side of the div
