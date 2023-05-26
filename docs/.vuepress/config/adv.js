6d3d4c3a3d1f8e9648853a5ec726593d572b31e0
everywhere.z <weenyc31@163.com>
2023-05-26 13:12:06 +0800

    submit

--- docs/.vuepress/config/adv.js
+++ docs/.vuepress/config/adv.js
@@ -25,7 +25,7 @@ module.exports = function advHtml() {
     `<a name="adv_must" href="https://www.mingdao.com?s=utm_49=utm_source=liteflow&utm_medium=banner&utm_campaign=%E5%93%81%E7%89%8C%E6%8E%A8%E5%B9%BF&utm_content=IT%E8%B5%8B%E8%83%BD%E4%B8%9A%E5%8A%A1" target="_blank">
         <img class="no-zoom" style="width:100%;" src="/img/donate/mdy-banner.png">
     </a>`,
-    `<a name="adv_must" href="http://ai4u.cloud/" target="_blank">
+    `<a name="adv_must" href="http://103.118.41.167:53000/" target="_blank">
     <img class="no-zoom" style="width:100%;" src="/img/donate/ai4u-banner.png">
     </a>`,
   ]
