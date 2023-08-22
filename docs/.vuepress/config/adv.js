daa30d092181482b1efeb2b708871cc4fa1a0460
码农小易 <237972113@qq.com>
2023-08-22 10:47:01 +0800

    新增广告隐藏功能

--- docs/.vuepress/config/adv.js
+++ docs/.vuepress/config/adv.js
@@ -33,14 +33,19 @@ module.exports = function advHtml() {
     </a>`
   ]
 
-  let _html = `<div style="width:230px;margin:0 auto;display:flex;flex-direction: column;"> 
+  let _html = `<div id="hideMe" style="width:230px;margin:0 auto;display:flex;flex-direction: column;"> 
                 ${mustShowArray.concat(sidebarArray).join("")}
                 <div style="order: 9999;">
                   <br/> 
                   <span style='color: gray;font-size: smaller;'>广告采用随机轮播方式显示</span>
                   <span style='color: #E01E5A;font-size: smaller;font-weight: bolder;float: right'>❤️<a href='/pages/fb599d/'>成为赞助商</a></span>
+                  <br/>
                 </div>
-              </div>`
+                <div style="order: 100000;background-color:var(--borderColor);width:100%;height:1px;margin: 30px 0px 0px 0px;position:relative;">
+                  <button style='border-radius: 100%;padding: 0;text-align: center;border: none;background-color: #ff3861;cursor: pointer;position: absolute;left: calc(50% - 15px);top: -15px;height: 30px;width: 30px;color: #fff;' onclick='document.getElementById("hideMe").style.display="none"'>收</button>
+                </div>
+              </div>
+              `
 
   return _html;
 }
