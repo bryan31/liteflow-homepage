cdb7023f7b29308ca15b97d38ac11e89381eb564
码农小易 <237972113@qq.com>
2023-10-17 00:08:44 +0800

    新增i18n翻译

---
+++ docs/.vuepress/config/en-us/nav.js
@@ -0,0 +1,59 @@
+module.exports = [
+    { text: '💒 Home', link: '/en-us/' },
+    { text: '📚 Document', link: '/pages/5816c5/', items: [
+            { text: 'v2.11.1(Current)', link: '/pages/5816c5/'},
+            { text: 'What\'s New', items: [
+                    { text: 'What\' s New In LiteFlow v2.11.1?', link: '/pages/8ff014/'},
+                ]
+            },
+            { text: 'History Versions', items: [
+                    { text: 'v2.10.X', link: '/pages/v2.10.X/5816c5/'},
+                    { text: 'v2.9.X', link: '/pages/v2.9.X/5816c5/'},
+                    { text: 'v2.8.X', link: '/pages/v2.8.X/5816c5/'},
+                    { text: 'v2.7.X', link: '/pages/v2.7.X/967466/'},
+                    { text: 'v2.6.X', link: '/pages/v2.6.X/dfd970/'},
+                ]
+            },
+            { text: 'Upgrade Guide', items: [
+                    { text: '2.9.3', link: '/pages/88c2f4/'},
+                    { text: '2.9.X', link: '/pages/88c2f3/'},
+                    { text: '2.8.X', link: '/pages/88c2e2/'},
+                    { text: '2.7.X', link: '/pages/4848b9/'},
+                ]
+            },
+        ]
+    },
+    { text: '❓ FQA', items: [
+            { text: 'FQA', link: '/pages/845dff/'},
+            { text: 'Q&A on Special Topics', items: [
+                    { text: 'How to Understand the Concept of LiteFlow Context?', link: '/pages/e1e61f/'},
+                    { text: 'What is the concept of Slot and what role does it play in LiteFlow?', link: '/pages/ad60b4/'},
+                ]
+            },
+        ]
+    },
+    { text: '💖 Sponsor', link: '/pages/fb599d/' },
+    { text: '🧩 Plugin', link: '/pages/liteflowx/' },
+    { text: '🔥 Powerpoint', link: '/pages/8d6666/' },
+    { text: '🐶 About', items: [
+            { text: 'LiteFlow', link: '/pages/8d7f1e/'},
+            { text: 'Team', link: '/pages/ae4d66/'},
+            { text: 'Changelog', link: '/pages/88c2d1/'},
+            { text: 'Be a contributor', link: '/pages/ae4dd5/'},
+        ]
+    },
+    // { text: '👥加入群聊', link: '/pages/73c2c3/' },
+    // { text: '🧤用户', link: '/pages/9b9f41/' },
+
+    { text: '🛖 Repository', items: [
+            { text: 'Gitee', link: 'https://gitee.com/dromara/liteFlow' },
+            { text: 'Github', link: 'https://github.com/dromara/liteflow' },
+            // { text: '推荐', items: [
+            //         { text: 'TLog-轻量级的分布式日志追踪神器', link: 'https://tlog.yomahub.com/'},
+            //         { text: 'ERD ONLINE-开箱即用的数据库建模、产品版本管理软件', link: 'https://portal.zerocode.net.cn/'},
+            //         { text: 'Forest-更轻量、更简单实用的HTTP客户端框架', link: 'https://forest.dtflyx.com/'}
+            //     ]
+            // },
+        ]
+    }
+];
